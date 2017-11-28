008f3200e5e8f4b7ed064eec8e182e1db629a9b7
Diana <dianaltecson@gmail.com>
2017-11-28 08:32:23 -0800

    prints list of products with their ids and prices

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,17 +9,33 @@ var connection = mysql.createConnection({
 // store user input here
 var searchQuery = 'Mouse';
 
-connection.query('SELECT * FROM `products` WHERE `product_name` = ?', searchQuery, function (error, results, fields) {
-    // error will be an Error if one occurred during the query
+// User prompts:
+// The first should ask them the ID of the product they would like to buy.
+// The second message should ask how many units of the product they would like to buy.
+// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+// If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
+// However, if your store does have enough of the product, you should fulfill the customer's order.
+
+// This means updating the SQL database to reflect the remaining quantity.
+// Once the update goes through, show the customer the total cost of their purchase.
+
+connection.query('SELECT * FROM `products`', function (error, results) {
+    // if there's an error, print and return
     if (error) {
         console.log(error);
         return;
     }
+    // first display all of the items available for sale. Include the ids, names, and prices of products for sale
+        // results from bamazon query is an object, with column names as properties, nested in an array
+        // iterate through array and print each of the objects' properties
+    // console.log('Results: ' + JSON.stringify(results));
+    console.log('Welcome to Bamazon! These are the items we currently have in stock: \n');
+    for (var i=0; i < results.length; i++) {
+        console.log('Item ID: ' + results[i].item_id);
+        console.log('Product Name: ' + results[i].product_name);
+        console.log('Price: $' + results[i].price);
+        console.log('--------------------------------------');
+    }
 
-    // results will contain the results of the query
-        // results from bamazon query is an object nested in an array with the column names as properties
-    console.log('Results: ' + JSON.stringify(results));
-
-    // fields will contain information about the returned results fields (if any)
-    // console.log('Fields: ' + JSON.stringify(fields));
   });
