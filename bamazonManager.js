b466cf0ddb8a27a78d25aeabd23790e52bc2a352
Diana <dianaltecson@gmail.com>
2017-11-29 09:43:11 -0800

    building function to increase stock quant

--- bamazonManager.js
+++ bamazonManager.js
@@ -26,8 +26,8 @@ function showManagerOps () {
             lowInventory();
         }
         else if (data.managerOp === 'Add to Inventory') {
-            console.log(data.managerOp);
-            
+            // display a prompt that will let the manager "add more" of any item currently in the store
+            increaseStock();
         }
         else if (data.managerOp === 'Add New Product') {
             console.log(data.managerOp);
@@ -80,4 +80,58 @@ function lowInventory () {
 
         showManagerOps();
       });
+}
+
+function increaseStock () {
+
+    connection.query('SELECT * FROM `products`', function (error, results) { 
+        if (error) {
+            console.log(error);
+            return;
+        }
+
+        var productNames = [];
+
+        for (var i=0; i < results.length; i++) {
+            productNames.push(results[i].product_name);
+        }
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                message: 'Which item would you like to replenish?',
+                choices: productNames,
+                name: 'addMore'
+            }
+        ]).then(function (data) {
+            var product = data.addMore;
+
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    message: 'How many units of ' + product + ' would you like to add to stock?',
+                    validate: function (value) {
+                        return isNaN(value) === false && parseInt(value) > 0
+                    },
+                    name: 'stockIncrease'
+                }
+            ]).then(function (data) {
+                var stockIncrease = parseInt(data.stockIncrease);
+                var currentStock;
+
+                connection.query('SELECT * FROM `products` WHERE ?', [{product_name: product}], function (error, results) {
+                    if (error) {
+                        console.log(error);
+                        return;
+                    }
+                    currentStock = results[0].stock_quantity;
+                    console.log(currentStock);
+                })
+
+                //     connection.query('UPDATE `products` SET stock_quantity = ? + stockIncrease WHERE product_name = ?', [])
+                // })
+    
+        })
+    })
+})
 }
